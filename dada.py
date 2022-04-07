1dabe996eca7ff6fa01a0e4717604d7b268cf299
dadapakistani <103193410+dadapakistani@users.noreply.github.com>
2022-04-07 18:10:53 +0500

    Update dada.py
    apt update
    apt upgrade
    pkg install git
    pkg install python2
    git clone https://github.com/dadapakistani/dadapakistani
    cd dadapakistani
    ls
    python2 dada.py

--- dada.py
+++ dada.py
@@ -47,8 +47,8 @@ logo = """
 \033[1;91m=======================================
 \033[1;96mAuthor  \033[1;93m: \033[1;92mlove
 \033[1;96mYouTube \033[1;93m: \033[1;92mPakistani Hackers
-\033[1;96mGitHub  \033[1;93m: \033[1;92mhttps://github.com/lovehacker/love
-\033[1;96mBlogger \033[1;93m: \033[1;92mhttps://www.facebook.com/lovehacker
+\033[1;96mGitHub  \033[1;93m: \033[1;92mhttps://github.com/dadapakistani/dadpakistani
+\033[1;96mBlogger \033[1;93m: \033[1;92mhttps://www.facebook.com/dadapakistani
 \033[1;91m======================================="""
 def tik():
 	titik = ['.   ','..  ','... ']
